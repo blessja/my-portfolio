2fb39c7704d0fb6a81565712f2c6da7a19309346
blessja <jaydenj772@gmail.com>
2021-12-03 23:35:07 +0200

    initial commit

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const projects = [
+  {
+    title: "GYM Body Builder",
+    subtitle: "Tailwind",
+    description:
+      "This Project was mainly build with Tailwind CSS.",
+    image: "./project-1.gif",
+    link: "",
+  },
+  {
+    title: "Task Tracker",
+    subtitle: "React",
+    description:
+      "The project is to track the tasks that needs to be completed",
+    image: "./project-2.gif",
+    link: "",
+  },
+  {
+    title: "Todo app",
+    subtitle: "React and Node.js",
+    description:
+     "A basic todo web application to keep all todo's",
+    image: "./project-3.gif",
+    link: "",
+  },
+  {
+    title: "Progress bar",
+    subtitle: "HTML, CSS & JAVASCRIPT",
+    description:
+      "This is a pure vanilla html, css & javascript project.",
+    image: "./project-4.gif",
+    link: "",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Express js",
+  "Node",
+  "Ruby on Rails",
+  "Material UI",
+  "HTML5",
+  "CSS3",
+];
